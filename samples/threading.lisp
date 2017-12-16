0110c5647e914ef617e53891bd73251fd5bc3f13
Julian 'Andrakis' Thatcher <daedalus.raistlin@gmail.com>
2017-12-17 01:45:02 +1100

    Explain at a detailed level how the new threading functions work

--- samples/threading.lisp
+++ samples/threading.lisp
@@ -1,22 +1,31 @@
 ;; Microthreading example
+;;
+;; This example demonstrates:
+;;  o  Starting microthreads with arguments
+;;  o  Sending messages between microthreads
+;;  o  Waiting for messages before code execution continues
+;;  o  Callbacks for message wait timeouts
+;;
+;; Key types::
+;;  o  thread_ref()    A reference to a thread
+;; Key functions:
+;;  o  (send Message::any() Destination::thread_ref()) -> bool()
+;;     - (send "sending myself love" (self)) -> true
+;;     - (send "not around anymore" NonExistantThread) -> false
+;;  o  (spawn lambda(Args) Args::any()) -> thread_ref()
+;;     - (spawn (# () (print "Short lived thread"))) -> thread_ref()
+;;     - (spawn (# (Parent) (send short_lived Parent)) (self)) -> thread_ref()
+;;  o  (receive OnMessage::lambda(Message) timeout()) -> lambda::result
+;;     timeout() -> phrase 'infinity' |
+;;                  phrase 'after' Timeout::int() OnTimeout::lambda()
+;;     - (receive (# (Message) (print "Message:" Message))) -> nil
+;;     - (receive (# (Number) (+ 1 Number))) -> int()
+;;     - (receive (# (X Y) (+ X Y)) after 5000 (# () 0)) -> int()
 
 (begin
 
 	(define Self (self))
 	(print "Master Thread" Self " started")
-
-	;; A cheap appromixation of receive.
-	;; Actually, it's probably fairly CPU-intensive.
-	(define recv (# (OnMessage) (begin
-		;; Nice visualization of how message loop runs.
-		(print "(" (self) ".recv)")
-		(define Messages (flush (self) 1))
-		;; TODO: implement (empty List) check
-		(if (> (length Messages) 0)
-			(OnMessage (head Messages))
-			(recv OnMessage)
-		)
-	)))
 
 	(define Thread1 (spawn (# (Parent) (begin
 		(print "Thread1 starting up, self=" (self) ", parent=" Parent)
@@ -24,14 +33,14 @@
 		(send Message Parent)
 		(print "Sent a message to " Parent)
 		(print "Thread1 waiting for reply")
-		(recv (# (Reply) (begin
+		(receive (# (Reply) (begin
 			(print "Thread1 got reply: " Reply)
 		)))
 		(print "Thread1 exiting")
 	)) Self))
 
 	(print "Master Thread: Waiting for message")
-	(recv (# (Message) (begin
+	(receive (# (Message) (begin
 		(print (self) " got message: " Message)
 		(define ReplyAddress (head Message))
 		(print (self) " sending reply to " ReplyAddress)
@@ -45,7 +54,7 @@
 		(# (Message) (begin
 			(print (self) " got message when shouldnt: " Message)
 		))
-		after 20 (# () (begin
+		after 2000 (# () (begin
 			(print (self) " timeout occurred, success")
 		))
 	)
